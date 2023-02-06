c91f1e45de78034c9c9d2177ea68f1f42ee9e7a3
Panyawat_KRONGKITICHU <pan.kkt@gmail.com>
2023-02-06 10:15:17 +0700

    GET

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,36 @@
 
 const String baseUrl = "https://jsonplaceholder.typicode.com/";
 
+void GET_post()
+{
+  DynamicJsonDocument doc(2048);
+  const String url = baseUrl + "posts/1";
+  HTTPClient http;
+  http.begin(url);
+  int httpResponseCode = http.GET();
+
+  if (httpResponseCode >= 200 && httpResponseCode < 300)
+  {
+    Serial.print("HTTP ");
+    Serial.println(httpResponseCode);
+    String payload = http.getString();
+    deserializeJson(doc, payload);
+
+    Serial.println();
+    Serial.println((const char *)doc["title"]);
+  }
+  else
+  {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+}
+
 void setup()
 {
   Serial.begin(115200);
   Connect_Wifi();
+  GET_post();
 }
 
 void loop()
